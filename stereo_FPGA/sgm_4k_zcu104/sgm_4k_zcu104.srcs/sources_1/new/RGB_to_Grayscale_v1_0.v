fc49d7900001fe94416f017f1381671138abc880
grabowskimariusz94 <71455615+grabowskimariusz94@users.noreply.github.com>
2021-02-19 19:56:47 +0100

    updated structure

--- stereo_FPGA/sgm_4k_zcu104/sgm_4k_zcu104.srcs/sources_1/new/RGB_to_Grayscale_v1_0.v
+++ stereo_FPGA/sgm_4k_zcu104/sgm_4k_zcu104.srcs/sources_1/new/RGB_to_Grayscale_v1_0.v
@@ -11,7 +11,6 @@
 	)
 	(
         input wire  aclk,
-		input wire  aresetn,
 
 		// Ports of Axi Slave Bus Interface S00_AXIS
 		output wire  s_axis_rgb_tready,
--- stereo_FPGA/sgm_4k_zcu104/sgm_4k_zcu104.srcs/sources_1/new/SAD/SAD.sv
+++ stereo_FPGA/sgm_4k_zcu104/sgm_4k_zcu104.srcs/sources_1/new/SAD/SAD.sv
@@ -20,33 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module SAD(
+module SAD #
+    (
+        parameter CNTX_SIZE = 3,
+        parameter DATA_WIDTH = 8
+    )
+    (
         input clk,
-        input [2:0][2:0][7:0] i_kernel1_data,
-        input [2:0][2:0][7:0] i_kernel2_data,
+        input [CNTX_SIZE-1:0][CNTX_SIZE-1:0][DATA_WIDTH-1:0] i_kernel1_data,
+        input [CNTX_SIZE-1:0][CNTX_SIZE-1:0][DATA_WIDTH-1:0] i_kernel2_data,
         input i_kernels_valid,
-        output [7:0] o_sad_data
+        output reg [DATA_WIDTH-1:0] o_sad_data
     );
     
-    reg signed [2:0][2:0][8:0] sub_data;
-    reg [2:0][2:0][7:0] abs_data;
-    reg valid = 1'b0;
-    reg [11:0] sum_data;
-    reg [7:0] output_data;
+    reg signed [CNTX_SIZE-1:0][CNTX_SIZE-1:0][(DATA_WIDTH+1)-1:0] sub_data;
     
     always @(*)
     begin
-        for (int i=0;i<3;i=i+1) begin
-            for (int j=0;j<3;j=j+1)
-                sub_data[i][j] = {1'b0,i_kernel1_data[i][j][7:0]}-{1'b0, i_kernel2_data[i][j][7:0]};
+        for (int i=0;i<CNTX_SIZE;i=i+1) begin
+            for (int j=0;j<CNTX_SIZE;j=j+1)
+                sub_data[i][j] = i_kernel1_data[i][j][DATA_WIDTH-1:0]-i_kernel2_data[i][j][DATA_WIDTH-1:0];
         end
     end
     
+    reg [CNTX_SIZE-1:0][CNTX_SIZE-1:0][DATA_WIDTH-1:0] abs_data;
+    reg valid = 1'b0;
     always @(posedge clk)
     begin
-        for (int i=0;i<3;i=i+1) begin
-            for (int j=0;j<3;j=j+1) begin
-                if(sub_data[i][j][8])
+        for (int i=0;i<CNTX_SIZE;i=i+1) begin
+            for (int j=0;j<CNTX_SIZE;j=j+1) begin
+                if(sub_data[i][j][(DATA_WIDTH+1)-1])
                     abs_data[i][j] <= -sub_data[i][j];
                 else
                     abs_data[i][j] <= sub_data[i][j];
@@ -55,21 +58,19 @@ module SAD(
         valid <= i_kernels_valid;
     end
     
+    reg [(DATA_WIDTH+4)-1:0] sum_data;
     always @(*)
     begin
         sum_data = 0;
-        for (int i=0;i<3;i=i+1) begin
-            for (int j=0;j<3;j=j+1)
+        for (int i=0;i<CNTX_SIZE;i=i+1) begin
+            for (int j=0;j<CNTX_SIZE;j=j+1)
                 sum_data = sum_data + abs_data[i][j];
         end
     end
     
     always @(posedge clk)
     begin
-       output_data <= (sum_data[11:8]==4'b0000 || valid) ? sum_data[7-:8] : 8'hFF; //if not valid => FF
+       o_sad_data <= (sum_data[(DATA_WIDTH+4)-1-:4]==4'b0000 & valid) ? sum_data[DATA_WIDTH-1:0] : '1; //if not valid => all ones
     end
-    
-    assign o_sad_data = output_data;
-    //assign o_sad_valid = valid[1];
     
 endmodule
