d04d9483d55415c6d693d3caf59ada16a5e636a3
grabowskimariusz94 <71455615+grabowskimariusz94@users.noreply.github.com>
2021-03-18 22:22:51 +0100

    Initial IP for hardware in order to join the video path
    For now only the RGB to GRAY transformation

---
+++ stereo_FPGA/ip_repo/Stereovision/Stereovision_1.0/hdl/Stereovision_v1_0.v
@@ -0,0 +1,68 @@
+
+`timescale 1 ns / 1 ps
+
+	module Stereovision_v1_0 #
+	(
+		parameter WIDTH = 3840,
+        parameter HEIGHT = 2160,
+        parameter MAX_DISP = 255,
+        parameter CNTX_SIZE = 3,
+        parameter MAX_SAMPLES_PER_CLOCK = 4,
+    
+        parameter AXIS_TDATA_WIDTH = 96,
+        parameter DATA_WIDTH = 8
+	)
+	(
+		input wire  aclk,
+		input wire  aresetn,
+		
+		output wire  s_axis_tready,
+		input wire [AXIS_TDATA_WIDTH-1 : 0] s_axis_tdata,
+		input wire s_axis_tuser,
+		input wire  s_axis_tlast,
+		input wire  s_axis_tvalid,
+
+		output wire  m_axis_tvalid,
+		output wire [AXIS_TDATA_WIDTH-1 : 0] m_axis_tdata,
+		output wire  m_axis_tuser,
+		output wire  m_axis_tlast,
+		input wire  m_axis_tready
+	);
+    
+    wire [AXIS_TDATA_WIDTH/3-1:0] m_axis_tdata_gray;
+
+    Stereovision
+    #(
+        .WIDTH(WIDTH),
+        .HEIGHT(HEIGHT),
+        .MAX_DISP(MAX_DISP),
+        .CNTX_SIZE(CNTX_SIZE),
+        .MAX_SAMPLES_PER_CLOCK(MAX_SAMPLES_PER_CLOCK),
+        .AXIS_TDATA_WIDTH(AXIS_TDATA_WIDTH/3),
+        .DATA_WIDTH(DATA_WIDTH)
+    ) stereo
+    (
+        .aclk(aclk),
+
+		// Ports of Axi Slave Bus Interface S00_AXIS
+		.s_axis_img_tready(s_axis_tready),
+		.s_axis_img_tdata(s_axis_tdata),
+		.s_axis_img_tuser(s_axis_tuser),
+		.s_axis_img_tlast(s_axis_tlast),
+		.s_axis_img_tvalid(s_axis_tvalid),
+        
+		// Ports of Axi Master Bus Interface M00_AXIS
+		.m_axis_disp_tvalid(m_axis_tvalid),
+		.m_axis_disp_tdata(m_axis_tdata_gray),
+		.m_axis_disp_tuser(m_axis_tuser),
+		.m_axis_disp_tlast(m_axis_tlast),
+		.m_axis_disp_tready(m_axis_tready)
+    );
+    
+    for(genvar i = MAX_SAMPLES_PER_CLOCK; i > 0; i = i - 1) begin
+       assign m_axis_tdata[i * 3 * DATA_WIDTH - 0 * DATA_WIDTH - 1 -: DATA_WIDTH] = m_axis_tdata_gray[i*DATA_WIDTH-1-:DATA_WIDTH];
+       assign m_axis_tdata[i * 3 * DATA_WIDTH - 2 * DATA_WIDTH - 1 -: DATA_WIDTH] = m_axis_tdata_gray[i*DATA_WIDTH-1-:DATA_WIDTH];
+       assign m_axis_tdata[i * 3 * DATA_WIDTH - 1 * DATA_WIDTH - 1 -: DATA_WIDTH] = m_axis_tdata_gray[i*DATA_WIDTH-1-:DATA_WIDTH];
+    end
+ 
+    endmodule
---
+++ stereo_FPGA/ip_repo/Stereovision/Stereovision_1.0/xgui/Stereovision_v1_0.tcl
@@ -0,0 +1,115 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  ipgui::add_page $IPINST -name "Page 0"
+
+  ipgui::add_param $IPINST -name "WIDTH"
+  ipgui::add_param $IPINST -name "HEIGHT"
+  ipgui::add_param $IPINST -name "MAX_DISP"
+  ipgui::add_param $IPINST -name "CNTX_SIZE"
+  ipgui::add_param $IPINST -name "MAX_SAMPLES_PER_CLOCK"
+  ipgui::add_param $IPINST -name "AXIS_TDATA_WIDTH"
+  ipgui::add_param $IPINST -name "DATA_WIDTH"
+
+}
+
+proc update_PARAM_VALUE.AXIS_TDATA_WIDTH { PARAM_VALUE.AXIS_TDATA_WIDTH } {
+	# Procedure called to update AXIS_TDATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.AXIS_TDATA_WIDTH { PARAM_VALUE.AXIS_TDATA_WIDTH } {
+	# Procedure called to validate AXIS_TDATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.CNTX_SIZE { PARAM_VALUE.CNTX_SIZE } {
+	# Procedure called to update CNTX_SIZE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.CNTX_SIZE { PARAM_VALUE.CNTX_SIZE } {
+	# Procedure called to validate CNTX_SIZE
+	return true
+}
+
+proc update_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to update DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DATA_WIDTH { PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to validate DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.HEIGHT { PARAM_VALUE.HEIGHT } {
+	# Procedure called to update HEIGHT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.HEIGHT { PARAM_VALUE.HEIGHT } {
+	# Procedure called to validate HEIGHT
+	return true
+}
+
+proc update_PARAM_VALUE.MAX_DISP { PARAM_VALUE.MAX_DISP } {
+	# Procedure called to update MAX_DISP when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.MAX_DISP { PARAM_VALUE.MAX_DISP } {
+	# Procedure called to validate MAX_DISP
+	return true
+}
+
+proc update_PARAM_VALUE.MAX_SAMPLES_PER_CLOCK { PARAM_VALUE.MAX_SAMPLES_PER_CLOCK } {
+	# Procedure called to update MAX_SAMPLES_PER_CLOCK when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.MAX_SAMPLES_PER_CLOCK { PARAM_VALUE.MAX_SAMPLES_PER_CLOCK } {
+	# Procedure called to validate MAX_SAMPLES_PER_CLOCK
+	return true
+}
+
+proc update_PARAM_VALUE.WIDTH { PARAM_VALUE.WIDTH } {
+	# Procedure called to update WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.WIDTH { PARAM_VALUE.WIDTH } {
+	# Procedure called to validate WIDTH
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.WIDTH { MODELPARAM_VALUE.WIDTH PARAM_VALUE.WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.WIDTH}] ${MODELPARAM_VALUE.WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.HEIGHT { MODELPARAM_VALUE.HEIGHT PARAM_VALUE.HEIGHT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.HEIGHT}] ${MODELPARAM_VALUE.HEIGHT}
+}
+
+proc update_MODELPARAM_VALUE.MAX_DISP { MODELPARAM_VALUE.MAX_DISP PARAM_VALUE.MAX_DISP } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.MAX_DISP}] ${MODELPARAM_VALUE.MAX_DISP}
+}
+
+proc update_MODELPARAM_VALUE.CNTX_SIZE { MODELPARAM_VALUE.CNTX_SIZE PARAM_VALUE.CNTX_SIZE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.CNTX_SIZE}] ${MODELPARAM_VALUE.CNTX_SIZE}
+}
+
+proc update_MODELPARAM_VALUE.MAX_SAMPLES_PER_CLOCK { MODELPARAM_VALUE.MAX_SAMPLES_PER_CLOCK PARAM_VALUE.MAX_SAMPLES_PER_CLOCK } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.MAX_SAMPLES_PER_CLOCK}] ${MODELPARAM_VALUE.MAX_SAMPLES_PER_CLOCK}
+}
+
+proc update_MODELPARAM_VALUE.AXIS_TDATA_WIDTH { MODELPARAM_VALUE.AXIS_TDATA_WIDTH PARAM_VALUE.AXIS_TDATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.AXIS_TDATA_WIDTH}] ${MODELPARAM_VALUE.AXIS_TDATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.DATA_WIDTH { MODELPARAM_VALUE.DATA_WIDTH PARAM_VALUE.DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DATA_WIDTH}] ${MODELPARAM_VALUE.DATA_WIDTH}
+}
+
